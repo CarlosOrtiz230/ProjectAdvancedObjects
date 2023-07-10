e705b3c0cea6f3c337b81de993b7bebafbf39fa5
qhi6067 <jaime123perez43@gmail.com>
2023-07-09 23:41:16 -0600

    Customer Version 1 __ Jaime Perez

---
+++ Customer.java
@@ -0,0 +1,16 @@
+public class Customer extends Person {
+    private List<Account> accounts;
+
+    public Customer(String name, String address) {
+        super(name, address);
+        accounts = new ArrayList<>();
+    }
+
+    public void addAccount(Account account) {
+        accounts.add(account);
+    }
+
+    public List<Account> getAccounts() {
+        return accounts;
+    }
+}
