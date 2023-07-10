226cd9402e026f2b26b0fc3ec065a56e88616ee1
qhi6067 <jaime123perez43@gmail.com>
2023-07-09 23:41:50 -0600

    Bank Version 1 __ Jaime Perez

---
+++ Bank.java
@@ -0,0 +1,15 @@
+public class Bank {
+    private List<Customer> customers;
+
+    public Bank() {
+        customers = new ArrayList<>();
+    }
+
+    public void addCustomer(Customer customer) {
+        customers.add(customer);
+    }
+
+    public List<Customer> getCustomers() {
+        return customers;
+    }
+}
