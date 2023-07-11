1356e7add93bf9af7481eb19d0409cad565db893
qhi6067 <jaime123perez43@gmail.com>
2023-07-10 22:33:53 -0600

    Completed Version for Checkup Monday July 10th -- Jaime Perez

--- Main.java
+++ Main.java
@@ -8,29 +8,31 @@ public class Main {
     public static void main(String[] args) {
         String csvFile = "BankUser.CSV";
         String line;
-
         List<Customer> customers = new ArrayList<>();
 
         try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
             int skipfirst12 = 0;
-            br.readLine();
             while ((line = br.readLine()) != null) {
-
+                if(skipfirst12 < 12){ // skip first 12 data
+                    skipfirst12++;
+                    System.out.println(line);//testing
+                    continue;
+                }
                 String[] data = line.split(",");
 
                 String identificationNumber = data[0];
                 String firstName = data[1];
                 String lastName = data[2];
                 String dob = data[3];
-                String address = data[4];
-                String phoneNumber = data[5].replace("(", "").replace(")", "").replace("-", "").replace(" ", "");
-                String checkingAccountNum = data[6];
-                double checkingStartingBalance = Double.parseDouble(data[7]);
-                String savingsAccountNum = data[8];
-                double savingsStartingBalance = Double.parseDouble(data[9]);
-                String creditAccountNumber = data[10];
-                double creditMax = Double.parseDouble(data[11]);
-                double creditStartingBalance = Double.parseDouble(data[12]);
+                String address = data[4] + "," + data[5] + "," + data[6]; // Combine address
+                String phoneNumber = data[7].replace("(", "").replace(")", "").replace("-", "").replace(" ", "");
+                String checkingAccountNum = data[8];
+                double checkingStartingBalance = Double.parseDouble(data[9]);
+                String savingsAccountNum = data[10];
+                double savingsStartingBalance = Double.parseDouble(data[11]);
+                String creditAccountNumber = data[12];
+                double creditMax = Double.parseDouble(data[13]);
+                double creditStartingBalance = Double.parseDouble(data[14]);
 
                 Customer customer = new Customer(firstName + " " + lastName, address);
                 Checking checkingAccount = new Checking(checkingAccountNum);
